72ccd346401832e6a054a965fdb1bb9cb1e51ba7
Mick210 <sid222m@gmail.com>
2023-06-29 15:47:10 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -14,26 +14,31 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         byte age = 18;
-        if (age >= 18)
+        if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
-        else
+        } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         byte airTemperature = 0;
-        if (airTemperature >= 5)
+        if (airTemperature >= 5) {
             System.out.println("Сегодня тепло, можно идти без шапки.");
-        else System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         short speed = 220;
-        if (speed <= 60)
+        if (speed <= 60) {
             System.out.println("Если скорость " + speed + " можно ездить спокойно.");
-        else System.out.println("Если скорость " + speed + " придется заплатить штраф.");
+        } else {
+            System.out.println("Если скорость " + speed + " придется заплатить штраф.");
+        }
     }
 
     public static void task4() {
